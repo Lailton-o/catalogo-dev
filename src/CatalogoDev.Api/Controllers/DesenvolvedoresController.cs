03c38c1a8549309a02b56596bfcb47db06b69d69
Lailton Oliveira dos Santos <infobase.lsantos@terceiros.mag.com.br>
2023-11-05 22:49:34 -0300

    Criação de endpoints get para obtenção de desenvolvedores

---
+++ src/CatalogoDev.Api/Controllers/DesenvolvedoresController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoDev.Api.Controllers;
+
+[Route("api/desenvolvedores")]
+[ApiController]
+public class DesenvolvedoresController : ControllerBase
+{
+    private List<string> _desenvolvedores;
+
+    public DesenvolvedoresController()
+    {
+        _desenvolvedores = new ()
+        {
+            "Lailton", "Rafael", "Luiz", "Fernando", "Maria", "Alessandra", "Carlos"
+        };
+    }
+
+    [HttpGet]
+    public List<string> ListarDevs()
+    {
+        return _desenvolvedores;
+    }
+
+    [HttpGet("{id}")]
+    public string PegarDev(int id)
+    {
+        if (id > _desenvolvedores.LongCount() - 1) return "Id inválido";
+
+        return _desenvolvedores.ElementAt(id);
+    }
+}
